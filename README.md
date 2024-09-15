913fbed484b53656afc23245ba593af9d95b114a
vitor <vitorhugosilva2009@hotmail.com>
2024-09-14 21:51:29 -0300

    colocando bot em unico arquivo

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 Projeto para baixar o titulo, a descrição e a thumbnail dos vídeos do youtube
 dado um channel_id e a chave de api.
+Vcs também devem passar uma playlist_id para o script.
+Api do youtube é muito instável não estava trazendo todos os videos, o que eu fiz foi criar uma playlist publica no
+canal com todos os vídeos qeu eu queria.'
 Estou utilizando a api do youtube logo vc precisa entrar e cadastrar uma chave de api
 para fazer o projeto funcionar, se atente pq não é 100% free, passou de uma cota o youtube bloqueia e tem que pagar para
 liberar.
 
-Como rodar essa :bomba 
+Como rodar essa :bomba
 
 instala as dependencias.
 
--- recuperaVideos.py
+++ recuperaVideos.py
@@ -8,54 +8,60 @@ import unidecode
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
+linhas_remover = open("textoEliminarDescrição.txt", encoding='utf-8').readlines()
 load_dotenv()
 
 api_key = os.getenv('API_KEY')
 
 channel_id = os.getenv('CHANNEL_ID')
 
+youtube = build('youtube', 'v3', developerKey=api_key)
 
-def get_youtube_videos():
-    youtube = build('youtube', 'v3', developerKey=api_key)
+
+def get_all_videos():
+    url = 'https://www.googleapis.com/youtube/v3/playlistItems'
+    uploads_playlist_id = "PL1m32XfQHqh7fJyEeuftjqdjG_D0T29Vw"
+
     videos = []
     next_page_token = None
-    # api parece que se perde e retorna resultados repetidos, tenho que olhar com calma
-    # por hora isso resolve.
-    ja_inseridos = []
 
     while True:
-        search_request = youtube.search().list(
-            part='snippet',
-            channelId=channel_id,
-            type='video',
-            maxResults=100,
-            pageToken=next_page_token
-        )
-        search_response = search_request.execute()
+        params = {
+            'part': 'snippet,contentDetails',
+            'playlistId': uploads_playlist_id,
+            'maxResults': 50,
+            'pageToken': next_page_token,
+            'key': api_key
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        video_ids = [item['contentDetails']['videoId'] for item in data['items']]
+        videos.extend(video_ids)
 
-        video_ids = [item['id']['videoId'] for item in search_response.get('items', [])]
-
-        if not video_ids:
+        # Check if there is a next page
+        next_page_token = data.get('nextPageToken')
+        if not next_page_token:
             break
 
+    return videos
+
+
+def get_youtube_videos(videos_ids):
+    ja_inseridos = []
+    videos = []
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    for i in range(0, len(videos_ids), 50):
+        batch_ids = videos_ids[i:i + 50]
         videos_request = youtube.videos().list(
             part='snippet,contentDetails,liveStreamingDetails',
-            id=','.join(video_ids)
+            id=','.join(batch_ids)
         )
         videos_response = videos_request.execute()
 
         for video in videos_response.get('items', []):
             duration = video['contentDetails']['duration']
             duration_seconds = isodate.parse_duration(duration).total_seconds()
-            if 'liveStreamingDetails' in video.keys():
-                print("pulando video de live {0}".format(video['snippet']['title']))
-                continue
-
-            if duration_seconds < 120:
-                print("pulando video com duracao menor que 120 segundos aka shorts {0} "
-                      "duracao(s) e titulo '{1}'".
-                      format(duration_seconds, video['snippet']['title']))
-                continue
             if video['snippet']['title'] not in ja_inseridos:
                 video_data = {
                     'title': video['snippet']['title'],
@@ -67,11 +73,6 @@ def get_youtube_videos():
                 }
                 videos.append(video_data)
                 ja_inseridos.append(video['snippet']['title'])
-
-        next_page_token = search_response.get('nextPageToken')
-        if not next_page_token:
-            break
-
     return videos
 
 
@@ -88,8 +89,22 @@ def dowloadImage(url_image, dir_image):
         print("deu ruim pra pegar a imagem, pede ajuda pra alguém ")
 
 
+def limpa_descricao(descricao: str):
+    descricao_final = ""
+    for linha in descricao.split("\n"):
+        if linha.startswith("#"):
+            continue
+        resultado = re.sub(r'\d{1,2}:\d{2}(?:\s+[^\d\s]+)*', '', linha).strip()
+        for texto_remover in linhas_remover:
+            resultado = resultado.replace(texto_remover.replace("\n", ""), "")
+        if len(resultado) != 0:
+            descricao_final = descricao_final + resultado
+    return descricao_final
+
+
 def main():
-    videos = get_youtube_videos()
+    videos_ids = get_all_videos()
+    videos = get_youtube_videos(videos_ids)
     for video in videos:
         # gambiarra para tirar acento, lixo do windows tava reclamando
         video_titulo = unidecode.unidecode(str(video['title']).replace(" ", "-"))
@@ -98,7 +113,7 @@ def main():
         dowloadImage(video['thumbnail'], "videos/" + video_titulo)
         dados = {
             "titulo": video['title'],
-            "descrição": video['description'],
+            "descrição": limpa_descricao(video['description']),
             "video_id": video['videoId'],
             "url": "https://www.youtube.com/watch?v={0}".format(video['videoId'])
         }
@@ -109,5 +124,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# api do youtube é mto inconsistente.
